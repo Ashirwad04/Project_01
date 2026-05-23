9a570d9d4d12dcee221f6553d6fb020c08a9caa6
Ashirwad kumar <ashirwad@Ashirwads-MacBook-Air.local>
2026-05-23 17:05:40 +0530

    Branch01 changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,34 @@ import Header from "./Header";
 import Login,{Profile,Settings} from "./UserComponet";
 
 function App(){
+
+  const userObj={
+    name:"Aman",
+    age: 25,
+    city: "Delhi"
+  }
+
+
+  const name="aman";
+  let x=10;
+  let y= 20;
   return(
-   <div>
-    <Header/>
-    <h1>aman</h1>
-    <h2>karan</h2>
-    <Login/>
-    <Profile/>
-    <Settings/>
-    <ToDo/>
+  //  <div>
+  //   <Header/>
+  //   <h1>aman</h1>
+  //   <h2>karan</h2>
+  //   <Login/>
+  //   <Profile/>
+  //   <Settings/>
+  //   <ToDo/>
   
-   </div>
+  //  </div>
+  <div>
+    <h1>Welcome to My App</h1>
+    <h1>{name}</h1>
+    <h1>{x+y}</h1>
+    <h1>{userObj.age}</h1>
+  </div>
   )
 }
 
